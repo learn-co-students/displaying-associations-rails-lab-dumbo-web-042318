2b45115507c07d94aa004e5bf9fc922f1b2c7aba
travisdock <travisdock@gmail.com>
2018-05-22 06:22:59 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+artist1 = Artist.create(name: "Drake")
+artist2 = Artist.create(name: "J Balvin")
+
+song1 = Song.create(title: "God's Plan")
+song1.artist = artist1
+song1.save
+song2 = Song.create(title: "Nice for What")
+song2.artist = artist1
+song2.save
+song3 = Song.create(title: "Ginza")
+song3.artist = artist2
+song3.save
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
