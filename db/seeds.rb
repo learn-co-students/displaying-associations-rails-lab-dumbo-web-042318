0437d0f0dcc5f750604c699e087f15b843918e76
dbushey <polendeuva@gmail.com>
2018-05-23 17:51:46 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+the_strokes = Artist.create(name: "The Strokes")
+white_stripes = Artist.create(name: "White Stripes")
+
+last_night = Song.create(title: "Last Night")
+reptilia = Song.create(title: "Reptilia")
+
+seven_nation_army = Song.create(title: "Seven Nation Army")
+my_doorbell = Song.create(title: "My Doorbell")
+
+
+the_strokes.songs << last_night
+the_strokes.songs << reptilia
+
+white_stripes.songs << seven_nation_army
+white_stripes.songs << my_doorbell
